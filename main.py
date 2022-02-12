f8cb49c1604fac57d1c76cc184505f01a9cb36e5
Sven Müller <sven.mueller@aponia.de>
2022-02-13 00:21:50 +0100

    added readme for setup and deleted repo venv

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 from website import create_app
 
-app = create_app()
-
 if __name__ == '__main__':
-   app.run(debug=True)
-
-#from flask import Flask, render_template, url_for
-
-#app = Flask(__name__)
-
-
-#@app.route('/')
-#def index():
-#    return render_template("Instamash.html", image="static/img/nat2.png")
-
-
-#@app.route('/about')
-#def about():
-#    pass
-
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Setup
+## env:
+1. cd into .../Instamesh
+2. python -m venv virtual-env
+3. cd ./virtual-env/Scripts
+4. activate.bat
+5. pip install -r ../../requirements.txt
+6. cd ../..
+7. python main.py
